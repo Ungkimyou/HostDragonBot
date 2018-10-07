49fd5cf7884697a82e8d09d048e8d0c94c6725d8
Ungkimyou <39607284+Ungkimyou@users.noreply.github.com>
2018-10-07 08:04:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+// Discord.js bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    const args = msg.content.split(' ').slice(1).join(' ');
+    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
-client.login('process.env.TOKEN_BOT');
+
+client.login(process.env.TOKEN_BOT);
