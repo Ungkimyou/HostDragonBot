db543e790a6d1d8dfca6aa68ae4d22a8b987ef4d
Ungkimyou <39607284+Ungkimyou@users.noreply.github.com>
2018-10-07 16:55:35 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,8 +8,8 @@ let onlinelogs = bot.channels.get("496512276803747870");
 let online = new Discord.RichEmbed()
     .setTitle('Bot is Online NOw')
     .setColor("RANDOM")
-  //  .addField('Bot has restarted,', ` with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
-   // .setFooter(`${bot.user.tag}`) //FOOTER AND ICON
+    .addField('Bot has restarted,', ` with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`)
+    .setFooter(`${bot.user.tag}`) //FOOTER AND ICON
     .setTimestamp(); //SHOWS THAT COOL TIME ON THE FOOTER!
   onlinelogs.send(online);
 }
