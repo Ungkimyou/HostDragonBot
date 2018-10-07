867a636d87b5070e057b48b8c22f2c61d96eefc4
Ungkimyou <39607284+Ungkimyou@users.noreply.github.com>
2018-10-07 09:42:08 +0700

    Add files via upload

---
+++ commands/gamestats.js
@@ -0,0 +1,23 @@
+ const Discord = require("discord.js");
+ const prefix = ("d!")
+module.exports.run = async (bot, message, args) => {
+ message.react("✅");
+let embed = new Discord.RichEmbed()
+.setColor('#16d6e0')
+.setTitle("Gamestats Menu 🎮")
+.addField("`" + prefix +"fortnite`", "show your fortnite stats")
+.addField("`" + prefix +"csgo`", "show your CS-GO stats")
+.addField("`" + prefix +"battlefield4`", "show your Battlefield 4 stats")
+.setTimestamp() //This sets the timestamp.
+.setFooter(`Requested by :${message.author.tag} `) 
+ 
+
+
+message.channel.send(embed);
+}
+
+module.exports.help = {
+  name: "statistics"
+}
+
+//embed,say,clear,emojilist,ban,kick,autoplaying,addrole,removerole,rolecolor,nickbot,invitelist
